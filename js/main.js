eb26ffe0a7b4ce32df82b51e3e5880082c74f287
FrancescoCapriolo <francesco.capriolo92@gmail.com>
2022-03-04 17:32:31 +0100

    completed exercice

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
+//creo una variabile e la prendo dalla griglia grande
 const gridElement = document.getElementById("grid");
-
-let miniCard = [];
-
-let easyGrid = document.querySelector("button");
 
 //arrow function che restituisce un div 
 const createSquare = () => {
@@ -10,35 +7,48 @@ const createSquare = () => {
     currentGrid.classList.add("box");
     return currentGrid;
 }
+// ogni volta che premo il bottone mi crea  le griglie
+document.getElementById('button').addEventListener('click', function () {
+    //resetto prima
+    gridElement.innerHTML = "";
+    //inizializzo il valore una variabile prendendolo dall'id html(select)
+    let grid = document.getElementById("input-grid").value;
+    //se il select è uguale all'input che ho dato allora mi creo...
+    if (grid == "easy") {
+        for (i = 1; i <= 100; i++) {
+            //creo una variabile uguale alla funzione
+            const gridSquare = createSquare();
+            //aggiungo il numero alla griglia
+            gridSquare.innerHTML += [i];
+            //appena premo un quadrato della griglia gli do un colore diverso(alternando con troggle)
+            gridSquare.addEventListener('click', function () {
+                this.classList.toggle('box-clicked');
+            })
+            //aggiungo un figlio lla griglia grande
+            gridElement.appendChild(gridSquare);
+        }
+        //uguale ma meno i
+    } else if (grid == "medium") {
+        for (i = 1; i <= 80; i++) {
+            const gridSquare = createSquare();
+            gridSquare.innerHTML += [i];
 
+            gridSquare.addEventListener('click', function () {
+                this.classList.toggle('box-clicked');
+            })
+            gridElement.appendChild(gridSquare);
 
-for (i = 1; i <= 100; i++) {
-    const gridSquare = createSquare();
-    gridSquare.innerHTML += [i];
+        }
+    } else { //uguale ma meno i
+        for (i = 1; i <= 50; i++) {
+            const gridSquare = createSquare();
+            gridSquare.innerHTML += [i];
 
-    gridSquare.addEventListener('click', function () {
-        this.classList.toggle('box-clicked');
-    })
-    gridElement.appendChild(gridSquare);
+            gridSquare.addEventListener('click', function () {
+                this.classList.toggle('box-clicked');
+            })
+            gridElement.appendChild(gridSquare);
 
-}
-
-/* for (i = 1; i <= 80; i++) {
-    const gridSquare = createSquare();
-
-    gridSquare.addEventListener('click', function () {
-        this.classList.toggle('box-clicked');
-    })
-    gridElement.appendChild(gridSquare);
-
-} */
-
-/* for (i = 1; i <= 50; i++) {
-    const gridSquare = createSquare();
-
-    gridSquare.addEventListener('click', function () {
-        this.classList.toggle('box-clicked');
-    })
-    gridElement.appendChild(gridSquare);
-
-} */
+        }
+    }
+});
