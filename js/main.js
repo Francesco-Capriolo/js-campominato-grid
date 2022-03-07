e634e734661443875c080d56b6b7ea5cd16be67d
FrancescoCapriolo <francesco.capriolo92@gmail.com>
2022-03-07 15:41:07 +0100

    correzione e creazione funzione per creare celle

--- js/main.js
+++ js/main.js
@@ -1,4 +1,58 @@
-//creo una variabile e la prendo dalla griglia grande
+//funzione per creare una nuova griglia
+function createGame() {
+    //prima bisogna resettare ogni volta che si crea una nuova griglia
+    document.querySelector("#grid").innerHTML = "";
+    //recupero il livello selezionato
+    document.getElementById("input-level").value;
+    // prima di generare le varie celle devo assegnare una variabile
+    // da poter dare le varie gradezze in maniera omogenea
+    let cellNumber = 0;
+    let cellsRow = 0;
+    //generare i vari tipi di livello con lo switch
+    switch (input - level) {
+        case easy:
+            cellNumber = 100;
+            //^ oppure fare cellsRow=10;
+            break;
+        case medium:
+            cellNumber = 81;
+            //^ oppure fare cellsRow=9;
+            break;
+        case hard:
+            cellNumber = 49;
+            //^ oppure fare cellsRow=7;
+            break;
+    }
+    //$ sqrt significa la radice quadrata di cellNumber
+    cellsRow = Math.sqrt(cellNumber);
+}
+
+
+document.getElementById("button").addEventListener("click", function () {
+    createGame()
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*                 creare griglie con for            */
+
+/* //creo una variabile e la prendo dalla griglia grande
 const gridElement = document.getElementById("grid");
 
 //arrow function che restituisce un div 
@@ -12,7 +66,7 @@ document.getElementById('button').addEventListener('click', function () {
     //resetto prima
     gridElement.innerHTML = "";
     //inizializzo il valore una variabile prendendolo dall'id html(select)
-    let grid = document.getElementById("input-grid").value;
+    let grid = document.getElementById("input-level").value;
     //se il select è uguale all'input che ho dato allora mi creo...
     if (grid == "easy") {
         for (i = 1; i <= 100; i++) {
@@ -51,4 +105,4 @@ document.getElementById('button').addEventListener('click', function () {
 
         }
     }
-});
+}); */
